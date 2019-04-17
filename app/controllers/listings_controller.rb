1c215fc85ca583f5bf779348d4e7ef9368eb0d2f
johnr <johnrubio93@gmail.com>
2019-04-17 14:55:40 +1000

    added controllers and methods and views

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,31 @@
+class ListingsController < ApplicationController
+  
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    render :plain
+  end
+end

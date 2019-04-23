8de0683a5179ef34e47970810e3f46f5492a80ec
johnr <johnrubio93@gmail.com>
2019-04-23 15:13:24 +1000

    added logout user

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,13 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destory]
+  before_action :authorize_user, only: [:edit, :update, :destory]
   before_action :set_breeds_and_sexes, only: [:new, :edit]
+  
 
   def create
-    @listing = Listing.create(listing_params)
+    @listing = current_user.listings.create(listing_params)
+    # @listing = Listing.create(listing_params)
     if @listing.errors.any?
       set_breeds_and_sexes
       render "new"
@@ -48,6 +51,12 @@ class ListingsController < ApplicationController
   def set_listing
     id = params[:id]
     @listing = Listing.find(id)
+  end
+
+  def authorize_user
+    if @listing.user_id != current_user.id
+      redirect_to listings_path
+    end
   end
 
   def listing_params

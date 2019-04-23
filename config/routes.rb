45706d304297f89e8085c7f6086e6c55213464d7
johnr <johnrubio93@gmail.com>
2019-04-23 13:00:23 +1000

    installed and setup devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#home", as: "root"
   resources :listings
